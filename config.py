7f73dc9702a6feb01205e338f95882518badf036
TheCGaming <tiidezorder@yahoo.com>
2017-11-20 06:00:21 -1000

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 # configure the dm text
 def generate_dm_text(name):
-	return '''Hey {}, It great to connect
-			with you on twitter'''.format(name)
+	return '''Thank you for following us. Join our server: facs.voltagemcbe.cf:19132 . For MCBE and MCWIN10. Buy ranks on: http://vmcbe.buycraft.net and vote for us on http://plu.sh/vpevote . Also check out your webiste: www.voltagemcbe.cf . Thank you.'''.format(name)
 
-scheduler_time = 15 #in minutes
+scheduler_time = 2 #in minutes
 
-tw_username = "paraazz" #change this to yours
+tw_username = "VoltageMCBE" #change this to yours
